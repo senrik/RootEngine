4b0ec4ebcd525a041727c92191b5cedc92db4da0
Swanson, Eri <eri.swanson@gtri.gatech.edu>
2023-12-11 16:43:10 -0500

    Added a transversing test.

--- Testing/test_main.cpp
+++ Testing/test_main.cpp
@@ -7,40 +7,43 @@ using namespace std;
 struct Area {
 	int number;
 	Area* neighbors[4];
+
+	friend ostream& operator<<(ostream&, const Area&);
 };
 
-enum CompassEnum {
-	None = 0,
-	North = 1,
-	South = 2,
-	East = 3,
-	West = 4,
-};
-
-ostream& operator <<(ostream& out, Area _area) {
-	/*      0
-	*       ^
-	*       |
-	* -3 < - O - > -2
-	*       |
-	*       v
-	*       1
-	* 
+ostream& operator <<(ostream& out, const Area& _area) {
+	/*       [0]
+	*         ^
+	*         |
+	* [3] < - O - > [2]
+	*         |
+	*         v
+	*        [1]
+	*
 	*/
 	out << format("       {}       \n       ^\n       |\n{} < - {} - > {}\n       |\n       v\n       {}\n",
 		(_area.neighbors[0] != nullptr) ? _area.neighbors[0]->number : -1,
-		(_area.neighbors[1] != nullptr) ? _area.neighbors[1]->number : -1,
+		(_area.neighbors[3] != nullptr) ? _area.neighbors[3]->number : -1,
 		_area.number,
 		(_area.neighbors[2] != nullptr) ? _area.neighbors[2]->number : -1,
-		(_area.neighbors[3] != nullptr) ? _area.neighbors[3]->number : -1);
+		(_area.neighbors[1] != nullptr) ? _area.neighbors[1]->number : -1);
 	return out;
 }
 
+enum CompassEnum {
+	
+	North = 0,
+	South = 1,
+	East = 2,
+	West = 3,
+};
+
 ostream& operator <<(ostream& strm, CompassEnum compass) {
-	const string nameCompass[] = { "None", "North", "South", "East", "West" };
+	const string nameCompass[] = { "North", "South", "East", "West" };
 
 	return strm << nameCompass[compass];
 }
+
 
 int main(int argc, char* argv[]) {
 
@@ -78,12 +81,15 @@ int main(int argc, char* argv[]) {
 		testVector.erase(ptr);
 		ptr = testVector.begin();
 	}
+	/*
+	* Map Creation
+	*/
 	int mapSize = -1;
 	cout << "Input size of the map: ";
 	cin >> mapSize;
 	Area* root = new Area();
 	root->number = 0;
-	cout << *root;
+	//cout << *root;
 	for (int i = 0; i < 4; i++)
 	{
 		root->neighbors[i] = nullptr;
@@ -108,7 +114,7 @@ int main(int argc, char* argv[]) {
 		* North and East are + 1, South and West are -1
 		*/
 		if (_temp->neighbors[next] == nullptr) {
-			cout << "Linking " << (CompassEnum)(next + 1) << endl;
+			//cout << "Linking " << (CompassEnum)(next) << endl;
 			_temp->neighbors[next] = new Area();
 			_temp->neighbors[next]->number = count;
 			if (next % 2 == 0) {
@@ -117,7 +123,7 @@ int main(int argc, char* argv[]) {
 			else {
 				_temp->neighbors[next]->neighbors[next - 1] = _temp;
 			}
-
+			//cout << *_temp;
 			count++;
 		}
 
@@ -126,35 +132,59 @@ int main(int argc, char* argv[]) {
 		if (rand() % 10 > 4)
 		{
 			_temp = _temp->neighbors[next];
-			cout << "Moving " << (CompassEnum)(next + 1) << endl;
+			//cout << "Moving " << (CompassEnum)(next)  << " (" << _temp->number <<")" << endl;
 			resetNext = true;
 		}
-		next++;
+		if (next > 3) {
+			next = 0;
+		}
+		else {
+			next++;
+		}		
+	} // map created
+	/*
+	* Create a way to "traverse" the map
+	*/
+	int exitCode = -1;
+	Area* currentArea = root;
+	while (exitCode < 0) {
+		string _input;
+		cout << "You look aound and see a number on the wall: \"" << currentArea->number << "\"\n";
+		cout << (*currentArea);
+		cout << "Input command: ";
+		cin >> _input;
+		try {
+			if (currentArea != nullptr) {
+				if (_input == "look") {
+					cout << "You look aound and see a number on the wall: \"" << currentArea->number << "\"\n";
+					cout << (*currentArea);
+				}
+				else if (_input == "move") {
+					int _areaNum = -1;
+					cout << (*currentArea);
+					cout << "Input area to move to: ";
+					cin >> _areaNum;
+					
+					for (int i = 0; i < 4; i++) {
+						if (currentArea->neighbors[i] != nullptr && currentArea->neighbors[i]->number == _areaNum) {
+							currentArea = currentArea->neighbors[i];
+							break;
+						}
+					}
+					
 
-		//while (_temp->neighbors[next] != nullptr) {
-		//	// This has a tendency to get stuck here, find a better solution
-		//	// Just have them be initialized sequencially, and have the random chance to move?
-		//	int next = rand() % 4;
-		//	
-		//}
-				/*switch (next)
-		{
-		case 0:
-			_temp->neighbors[next]->neighbors[next + 1] = _temp;
-			break;
-		case 1:
-			_temp->neighbors[next]->neighbors[next - 1] = _temp;
-			break;
-		case 2:
-			_temp->neighbors[next]->neighbors[next + 1] = _temp;
-			break;
-		case 3:
-			_temp->neighbors[next]->neighbors[next - 1] = _temp;
-			break;
-		}*/
-		
-		
+				}
+				else if(_input == "exit") {
+					exitCode = 0;
+				}
+			}
+			
+		}
+		catch (...) {
+			// invalid command
+		}
 		
 	}
-	return 0;
+
+	return exitCode;
 }
