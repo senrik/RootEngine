160090a47fdc4b6e006c4b2da374ef36478e6017
Swanson, Eri <eri.swanson@gtri.gatech.edu>
2024-01-31 11:13:07 -0500

    Baseline OpenGL 3.3 render window.

---
+++ Render Pipeline/render_pipeline.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <glad/glad.h> //manages function pointers for OpenGL
+#include <GLFW/glfw3.h> // Abstraction layer for targeting multiple systems with OpenGL
+
+#pragma region Callback functions Prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+#pragma endregion
+
+void processInput(GLFWwindow* window);
+
+int main(int argc, char* argv[]) {
+
+#pragma region Boiler Plate
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGl", NULL, NULL);
+	if (window == NULL) {
+		printf("Failed to create GLFW window.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		printf("Failed to initialize GLAD.\n");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+#pragma endregion
+
+	while (!glfwWindowShouldClose(window)) {
+
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+#pragma region Callback functions Implimentations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+#pragma endregion
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
