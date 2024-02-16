559fa7183db2ccf73768394dda765ed44c621b6d
Swanson, Eri <eri.swanson@gtri.gatech.edu>
2024-02-16 13:41:23 -0500

    Camera uses quaternion rotation, and position.

--- Render Pipeline/Camera.cpp
+++ Render Pipeline/Camera.cpp
@@ -1,36 +1,29 @@
 #include "Camera.hpp"
 
 Camera::Camera() {
-	position = glm::vec3(0.0f, 0.0f, -1.0f);
-	target = glm::vec3(0.0f, 0.0f, 0.0f);
-	direction = glm::normalize(target - position);
-	// global up
-	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-	rightAxis = glm::normalize(glm::cross(up, direction));
-	upAxis = glm::normalize(glm::cross(direction, rightAxis));
-	rotationSpeed = 30.0f;
-};
-
-void Camera::SetDirection(const glm::vec3* _dir) {
-	direction = *_dir;
-}
-
-glm::vec3 Camera::GetUpAxis() {
-	return upAxis;
-}
-
-glm::vec3 Camera::GetRightAxis() {
-	return rightAxis;
-}
-
-void Camera_Update(Camera* _cam) {
-}
-
-// update the direction _dir[0] is the rotation around the x axis _dir[1] is the rotation around the y axis
-void OrbitCamera_Rotate(Camera* _cam, const float _dir[], const float deltaTime) {
-	// the rotation will move the camera to a new location
-	_cam->position = glm::vec3(0.0f, 0.0f, 0.0f);
-	// rotate the camera around the global y-axis (up)
+	position = glm::vec3(0.0f, 0.0f, -5.0f);
+	target = glm::vec3(0.0f);
 	
-	// rotate the cmaera around its own right axis
+	direction = glm::normalize(target - position);
+	upAxis = rotation * glm::vec3(0.0f, 1.0f, 0.0f);
+	rightAxis = rotation * glm::vec3(1.0f, 0.0f, 0.0f);
+	rotationSpeed = 5.0f;
+
+}
+
+float Camera::GetRotationSpeed() {
+	return rotationSpeed;
+}
+
+void Camera::UpdateRotation(glm::quat _newRot) {
+	rotation = _newRot* rotation ;
+}
+
+void Camera_Rotate(Camera* _cam, glm::vec3 _axis, const float deltaTime) {
+	_cam->UpdateRotation(glm::quat(glm::normalize(_axis) * deltaTime * _cam->GetRotationSpeed()));
+}
+
+glm::mat4 Camera::GetViewMatrix() {
+	
+	return glm::translate(glm::mat4(1.0f), position) * glm::mat4_cast(rotation);
 }
--- Render Pipeline/Camera.hpp
+++ Render Pipeline/Camera.hpp
@@ -1,18 +1,23 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Camera {
 public:
 	Camera();
-	glm::vec3 position, target;
-	glm::vec3 GetDirection();
-	void SetDirection(const glm::vec3*);
+	//glm::vec3 GetDirection();
+	//void SetDirection(const glm::vec3*);
 	glm::vec3 GetRightAxis();
 	glm::vec3 GetUpAxis();
+	glm::mat4 GetViewMatrix();
+	void UpdateRotation(glm::quat);
+	float GetRotationSpeed();
 private:
-	glm::vec3 direction;// direction -- quaternion?
-	glm::vec3 rightAxis, upAxis;
+	glm::vec3 position, target;
+	glm::vec3 direction, rightAxis, upAxis;
+	glm::quat rotation;
 	float rotationSpeed;
 	// translate camera 
 	// - WASD based off of the orientation of the camera
@@ -22,7 +27,7 @@ private:
 	// - store as a unit vector
 };
 
-void Camera_Update(Camera*);
+void Camera_Rotate(Camera*, glm::vec3, const float);
 void OrbitCamera_Rotate(Camera*, const float[], const float);
 
 #endif // !CAMERA_H
