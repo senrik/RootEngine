b05af987561853cca42d195172208d148af5b003
Swanson, Eri <eri.swanson@gtri.gatech.edu>
2024-02-19 17:30:04 -0500

    Added controls for translating and rotating the camera.

--- Render Pipeline/Camera.cpp
+++ Render Pipeline/Camera.cpp
@@ -8,19 +8,45 @@ Camera::Camera() {
 	upAxis = rotation * glm::vec3(0.0f, 1.0f, 0.0f);
 	rightAxis = rotation * glm::vec3(1.0f, 0.0f, 0.0f);
 	rotationSpeed = 5.0f;
-
+	translationSpeed = 2.0f;
 }
 
 float Camera::GetRotationSpeed() {
 	return rotationSpeed;
 }
 
+float Camera::GetTranslationSpeed() {
+	return translationSpeed;
+}
+
 void Camera::UpdateRotation(glm::quat _newRot) {
-	rotation = _newRot* rotation ;
+	rotation = rotation * _newRot;
+
+	upAxis = rotation * glm::vec3(0.0f, 1.0f, 0.0f);
+	rightAxis = rotation * glm::vec3(1.0f, 0.0f, 0.0f);
+}
+
+void Camera::SetRotation(glm::quat _rot) {
+	rotation = _rot;
+	upAxis = rotation * glm::vec3(0.0f, 1.0f, 0.0f);
+	rightAxis = rotation * glm::vec3(1.0f, 0.0f, 0.0f);
+}
+
+void Camera::UpdatePosition(glm::vec3 _vec) {
+	//position += rotation *_vec;
+	//target += rotation* _vec;
 }
 
 void Camera_Rotate(Camera* _cam, glm::vec3 _axis, const float deltaTime) {
 	_cam->UpdateRotation(glm::quat(glm::normalize(_axis) * deltaTime * _cam->GetRotationSpeed()));
+}
+
+void Camera_ResetRotation(Camera* _cam) {
+	_cam->SetRotation(glm::quat());
+}
+
+void Camera_Translate(Camera* _cam, glm::vec3 _axis, const float deltaTime) {
+	_cam->UpdatePosition(glm::normalize(_axis) * deltaTime * _cam->GetTranslationSpeed());
 }
 
 glm::mat4 Camera::GetViewMatrix() {
