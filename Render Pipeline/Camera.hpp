ce363823b6a86cdcbd9a7eb72ffa3956b7a84a1c
senrik <13647220+senrik@users.noreply.github.com>
2024-02-22 20:30:53 -0500

    Changes the Camera to a C-style struct.

--- Render Pipeline/Camera.hpp
+++ Render Pipeline/Camera.hpp
@@ -2,27 +2,20 @@
 #define CAMERA_H
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-class Camera {
-public:
-	Camera();
-	glm::vec3 GetDirection();
-	void SetDirection(const glm::vec3*);
-	glm::vec3 GetRightAxis();
-	glm::vec3 GetUpAxis();
-	void UpdateRotation(glm::quat);
-	void UpdatePosition(glm::vec3);
-	void SetRotation(glm::quat);
-	float GetRotationSpeed();
-	float GetTranslationSpeed();
-private:
-	glm::vec3 position, target;
-	glm::vec3 direction, rightAxis, upAxis;
+typedef struct Camera {
+	glm::vec3 position, target, forwardAxis, rightAxis, upAxis;
 	glm::quat rotation;
+	glm::mat4 viewMat;
 	float rotationSpeed, translationSpeed;
-};
+} Camera;
 
+void Camera_Init(Camera*);
+//void Camera_UpdateRotation(Camera*, glm::quat);
+//void Camera_UpdatePosition(Camera*, glm::vec3);
+glm::mat4 Camera_GetViewMatrix(Camera* _cam);
 void Camera_Rotate(Camera*, glm::vec3, const float);
 void Camera_ResetRotation(Camera*);
 void Camera_Translate(Camera*, glm::vec3, const float);
